c78f51a34bcefba993c5b56a2639c01584e123a6
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-20 10:11:19 -0600

    Definir menu principal

--- PrimerParcialPOO_GomezHernandez_00300723/src/Main.java
+++ PrimerParcialPOO_GomezHernandez_00300723/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static void ShowMainMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to: ElectroMart!");
+        System.out.println("Enter an option:");
+        System.out.println("[1] Add article to inventory\n[2] Display article information\n[3] Show all articles\n[4] Modify article information\n[0] Exit");
+
     }
 }
