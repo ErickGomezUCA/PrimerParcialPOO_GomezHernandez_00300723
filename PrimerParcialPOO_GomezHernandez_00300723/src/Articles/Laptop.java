181ed779c0b9639d8a2364c17d0064db75ca135c
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-20 10:00:47 -0600

    Crear archivo Laptop.java

---
+++ PrimerParcialPOO_GomezHernandez_00300723/src/Articles/Laptop.java
@@ -0,0 +1,12 @@
+package Articles;
+
+public class Laptop extends Article {
+    // Constructors
+    public Laptop() {
+    }
+
+    public Laptop(String name, String model, String description, float price) {
+        super(name, model, description, price);
+    }
+
+}
