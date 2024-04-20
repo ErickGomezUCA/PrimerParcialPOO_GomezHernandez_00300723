a4535dd05bb71ff9be9d2428f853051987421918
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-20 10:00:12 -0600

    Crear archivo Phone.java

---
+++ PrimerParcialPOO_GomezHernandez_00300723/src/Articles/Phone.java
@@ -0,0 +1,11 @@
+package Articles;
+
+public class Phone extends Article {
+    // Constructors
+    public Phone() {
+    }
+
+    public Phone(String name, String model, String description, float price) {
+        super(name, model, description, price);
+    }
+}
