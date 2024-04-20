0d4ff330e75c5e004719070bf011a61066f8aa92
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-20 10:20:28 -0600

    Definir opciones del menu principal

--- PrimerParcialPOO_GomezHernandez_00300723/src/Main.java
+++ PrimerParcialPOO_GomezHernandez_00300723/src/Main.java
@@ -10,10 +10,34 @@ public class Main {
         int option;
 
         System.out.println("Welcome to: ElectroMart!");
-        System.out.println("Enter an option:");
-        System.out.println("[1] Add article to inventory\n[2] Display article information\n[3] Show all articles\n[4] Modify article information\n[0] Exit");
-        option = promptInt();
-        
+
+        do {
+            System.out.println("Enter an option:");
+            System.out.println("[1] Add article to inventory\n[2] Display article information\n[3] Show all articles\n[4] Modify article information\n[0] Exit");
+            option = promptInt();
+
+            switch (option) {
+                case 0: // Exit
+                    System.out.println("Exiting... Thanks for using our services!");
+                    break;
+
+                case 1: // Create article
+                    break;
+
+                case 2: // Show article info
+                    break;
+
+                case 3: // Show all articles
+                    break;
+
+                case 4: // Update article
+                    break;
+
+                default:
+                    System.out.println("Error: Please enter a valid option");
+                    break;
+            }
+        } while (option != 0);
     }
 
     private static int promptInt() {
