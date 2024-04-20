45da2b26274c3e92943b238c2390c73493f1fb55
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-20 10:02:45 -0600

    Agregar constructores, getters y setters de Inventory

--- PrimerParcialPOO_GomezHernandez_00300723/src/Inventory/Inventory.java
+++ PrimerParcialPOO_GomezHernandez_00300723/src/Inventory/Inventory.java
@@ -1,5 +1,30 @@
 package Inventory;
 
-public class Inventory {
+import java.util.ArrayList;
+import Articles.Article;
 
+public class Inventory {
+    private ArrayList<Article> articlesList;
+
+    // Constructors
+
+    public Inventory() {
+        articlesList = new ArrayList<Article>();
+    }
+
+    public Inventory(ArrayList<Article> articlesList) {
+        this.articlesList = articlesList;
+    }
+
+    // Getters and Setters
+
+    public ArrayList<Article> getArticlesList() {
+        return articlesList;
+    }
+
+    public void setArticlesList(ArrayList<Article> articlesList) {
+        this.articlesList = articlesList;
+    }
+
+    
 }
